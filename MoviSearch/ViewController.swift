ed309ace081b4974bae7897df269ed9045402f53
Gunel Aydinova <gunel.aydinova@willowtreeapps.com>
2024-06-29 14:19:31 -0400

    Initial Commit

---
+++ MoviSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviSearch
+//
+//  Created by Gunel Aydinova on 6/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
